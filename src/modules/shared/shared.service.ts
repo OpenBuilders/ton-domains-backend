a27fe98c301137c39cb8d130d7b98db722fcca87
chakzefir <chakzefir@hotmail.com>
2022-10-19 18:03:59 +0300

    feat: update min bid

--- src/modules/shared/shared.service.ts
+++ src/modules/shared/shared.service.ts
@@ -6,28 +6,28 @@ export class SharedService {
   static getMinBidByName(domain: string): string {
     switch (domain.length) {
       case 4:
-        return '900000000000';
+        return '810000000000';
       // absolute min 100
       case 5:
-        return '450000000000';
+        return '405000000000';
       // absolute min 50
       case 6:
-        return '360000000000';
+        return '324000000000';
       // absolute min 40
       case 7:
-        return '270000000000';
+        return '243000000000';
       // absolute min 30
       case 8:
-        return '180000000000';
+        return '162000000000';
       // absolute min 20
       case 9:
-        return '90000000000';
+        return '81000000000';
       // absolute min 10
       case 10:
-        return '45000000000';
+        return '40500000000';
       // absolute min 5
       default:
-        return '9000000000';
+        return '8100000000';
       // absolute min 1
     }
   }
